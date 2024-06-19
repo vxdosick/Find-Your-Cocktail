632ec1d887d1e66165fc4dafb0c32491b49e1041
Vadym Foteniuk <vadumfotenuk@gmail.com>
2024-06-19 19:56:45 +0300

    MEVN update config

--- server/config.js
+++ server/config.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PORT = exports.MONGO = void 0;
-// export const MONGO = "mongodb://127.0.0.1:27017/FindYourCocktail?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6"
-exports.MONGO = 'mongodb+srv://vxdosick:AAA003Sq@cluster0.sodpfvn.mongodb.net/FindYourCocktail?retryWrites=true&w=majority';
+exports.MONGO = process.env.MONGO || "mongodb://127.0.0.1:27017/FindYourCocktail?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6";
 exports.PORT = process.env.PORT || 3000;
