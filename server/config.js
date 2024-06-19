c0c3c8b63294c553e3557225969d92a17482311e
Vadym Foteniuk <vadumfotenuk@gmail.com>
2024-06-19 20:14:56 +0300

    MEVN update .env

--- server/config.js
+++ server/config.js
@@ -1,5 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PORT = exports.MONGO = void 0;
-exports.MONGO = process.env.MONGO || "mongodb://127.0.0.1:27017/FindYourCocktail?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6";
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+exports.MONGO = process.env.MONGO;
+// || "mongodb://127.0.0.1:27017/FindYourCocktail?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6"
 exports.PORT = process.env.PORT || 3000;
--- server/dev/config.ts
+++ server/dev/config.ts
@@ -1,3 +1,8 @@
-export const MONGO = process.env.MONGO || "mongodb://127.0.0.1:27017/FindYourCocktail?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6"
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+export const MONGO = process.env.MONGO
+// || "mongodb://127.0.0.1:27017/FindYourCocktail?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6"
 
 export const PORT = process.env.PORT || 3000
