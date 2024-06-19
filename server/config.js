083beea6a9d627c98d4f58682a76afd9482132a7
Vadym Foteniuk <vadumfotenuk@gmail.com>
2024-06-19 19:39:42 +0300

    MEVN add cluster mongo

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PORT = exports.MONGO = void 0;
-exports.MONGO = "mongodb://127.0.0.1:27017/FindYourCocktail?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6";
+// export const MONGO = "mongodb://127.0.0.1:27017/FindYourCocktail?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6"
+exports.MONGO = 'mongodb+srv://vxdosick:AAA003Sq@cluster0.sodpfvn.mongodb.net/FindYourCocktail?retryWrites=true&w=majority';
 exports.PORT = process.env.PORT || 3000;
--- server/dev/config.ts
+++ server/dev/config.ts
@@ -1,2 +1,3 @@
-export const MONGO = "mongodb://127.0.0.1:27017/FindYourCocktail?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6"
+// export const MONGO = "mongodb://127.0.0.1:27017/FindYourCocktail?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6"
+export const MONGO = 'mongodb+srv://vxdosick:AAA003Sq@cluster0.sodpfvn.mongodb.net/FindYourCocktail?retryWrites=true&w=majority'
 export const PORT = process.env.PORT || 3000
